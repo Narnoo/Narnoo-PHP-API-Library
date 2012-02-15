040ea2d59048e5fef354fe5282697313ae9caeb5
cnlangzi <support@reakoo.com>
2012-02-16 04:10:05 +0800

    add classes for narnoo request

---
+++ narnoo/class-distributor-narnoo-request.php
@@ -0,0 +1,13 @@
+<?php
+namespace narnoo;
+
+require_once ('narnoo/class-narnoo-request.php');
+
+class DistributorNarnooRequest extends NarnooRequest {
+	
+	function __construct() {
+	
+	}
+}
+
+?>
---
+++ narnoo/class-operator-narnoo-request.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace narnoo;
+
+require_once ('narnoo/class-narnoo-request.php');
+
+class OperatorNarnooRequest extends NarnooRequest{
+	
+}
+
+?>
