dcfc27aef0b92b586c312c45da8def956c8f9619
Narnoocom <wellzie.james@gmail.com>
2012-04-02 11:08:11 +1000

    Added page styling

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <html>
 <head>
+<title>Narnoo PHP-API-SDK</title>
 <style type="text/css">
 #title {
+	font-family: Arial, Helvetica, sans-serif;
 	font-size: 18px;
+	font-weight: bold;
+	color: #666;
+}
 </style>
 </head>
 <body>
-
-	<ul>
-		<li><a href="demo-distributor.php">Distributor</a></li>
-		<li><a href="demo-operator.php">Operator</a></li>
+<div id="title">Narnoo SDK Demos</div>
+<div>
+<ul>
+		<li><a href="demo-distributor.php">Distributor Demos</a></li>
 	</ul>
-
 </div>
+<div id="title" style="margin-top:15px">Narnoo Icons</div>
+<img src="icons/icon-128.png" width="124" height="124" alt="icon"><img src="icons/icon-96.png" width="96" height="96" alt="icon"><img src="icons/icon-72.png" width="72" height="72" alt="icon"><img src="icons/icon-64.png" width="64" height="64" alt="icon"><img src="icons/icon-48.png" width="48" height="48" alt="icon"><img src="icons/icon-32.png" width="32" height="32" alt="icon"><img src="icons/icon-24.png" width="24" height="24" alt="icon"><img src="icons/icon-16.png" width="16" height="16" alt="icon"></div>
 </body>
 </html>
 
