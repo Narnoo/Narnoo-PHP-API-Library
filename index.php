9ce4ae296e8baa68724537634a10a2fc1fe16cc9
Narnoocom <wellzie.james@gmail.com>
2012-04-02 11:08:20 +1000

    page styling

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 	</ul>
 </div>
 <div id="title" style="margin-top:15px">Narnoo Icons</div>
+<div style="margin-top:15px">
 <img src="icons/icon-128.png" width="124" height="124" alt="icon"><img src="icons/icon-96.png" width="96" height="96" alt="icon"><img src="icons/icon-72.png" width="72" height="72" alt="icon"><img src="icons/icon-64.png" width="64" height="64" alt="icon"><img src="icons/icon-48.png" width="48" height="48" alt="icon"><img src="icons/icon-32.png" width="32" height="32" alt="icon"><img src="icons/icon-24.png" width="24" height="24" alt="icon"><img src="icons/icon-16.png" width="16" height="16" alt="icon"></div>
 </body>
 </html>
