fbedb25d8ec6fc256c739730b8a7cfc56bf04743
cnlangzi <support@reakoo.com>
2012-06-12 23:16:42 +0800

    Changed demo into sandbox.

--- demos/narnoo-operator-config.php
+++ demos/narnoo-operator-config.php
@@ -1,13 +1,11 @@
 <?php
 
 /*sandbox app_key and secret_key, please change into live ones before going to live */
-/* $app_key = "1000000000"; // operator App Key which they can get from their control panel after add application
+$app_key = "1000000000"; // operator App Key which they can get from their control panel after add application
 $secret_key = "3141c0bfc514c924da0ac9019384bc000af4285b"; // operator SecretKey which they can get from their control panel after add application
- */
-$app_key = "1336470987119";
-$secret_key = "3ef8c3e14a1e3836006aa0650282994fb03d9a67";
+
 
 define ('app_key',$app_key);
 define ('secret_key',$secret_key);
-define ('sandbox',false);
+define ('sandbox',true);
 ?>
